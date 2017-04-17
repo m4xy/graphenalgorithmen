cac4d2971d3b5e9ef76c1706fe2c1a1ceba20704
Max Arndt <max@eyrichshof.de>
2017-04-17 13:29:45 +0200

    add vis graph from file

--- src/main/java/de/fhws/fiw/mis/webapp/bean/IndexBean.java
+++ src/main/java/de/fhws/fiw/mis/webapp/bean/IndexBean.java
@@ -1,9 +1,13 @@
 package de.fhws.fiw.mis.webapp.bean;
 
+import de.fhws.fiw.mis.graph.UndirGraph;
+import de.fhws.fiw.mis.graph.io.exporter.GraphVisJSExporterImpl;
+import de.fhws.fiw.mis.graph.io.importer.GraphImporter;
+import de.fhws.fiw.mis.graph.io.importer.GraphImporterImpl;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
-import javax.servlet.ServletContext;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
@@ -14,13 +18,22 @@ import java.util.*;
 @ManagedBean(name = "indexBean", eager = true)
 @RequestScoped
 public class IndexBean {
+    private String externalContext;
+    private UndirGraph graph;
+    private GraphVisJSExporterImpl visExporter;
+
+    public IndexBean() {
+        this.externalContext = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/");
+        GraphImporter importer = new GraphImporterImpl(externalContext + "graphs");
+        graph = importer.importGraph("Baum.txt");
+        visExporter = new GraphVisJSExporterImpl(graph);
+    }
 
     public List<String> getFiles() {
         List<String> files = new LinkedList<>();
 
         try {
-            String path = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/");
-            Files.walk(Paths.get(path + "graphs")).forEach(filePath -> {
+            Files.walk(Paths.get(externalContext + "graphs")).forEach(filePath -> {
                 if (Files.isRegularFile(filePath)) {
                     files.add(filePath.getFileName().toString().substring(0, filePath.getFileName().toString().length() -4));
                 }
@@ -31,5 +44,12 @@ public class IndexBean {
         }
 
         return files;
+    }
+
+    public String getVisNodeDataSet() {
+        return visExporter.getNodeDataSet();
+    }
+    public String getVisEdgeDataSet() {
+        return visExporter.getEdgeDataSet();
     }
 }
